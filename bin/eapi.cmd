f09fc8cef94b1cf7c1cc99223691dc2da49d8508
Kerli Fawkes <87852021+kfawkes0@users.noreply.github.com>
2021-07-27 14:13:13 +0300

    Create eapi.cmd

---
+++ bin/eapi.cmd
@@ -0,0 +1,12 @@
+:metastr:n:'eapi';c:'niknils@github.com';l:'MIT';v:'0';d:'eapi uses as interpreter for .eapi files with json configuration'
+
+:program
+@goto main
+
+:main
+  @echo eapi: wrn: is empty script. it's body for /bin/eapi.cmd
+@goto END.
+
+:end.
+:return
+:exit
